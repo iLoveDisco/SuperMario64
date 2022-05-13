3a1bc3d710976b7133c3eea52af3ebdf8e2c0494
Eric Tu <erictu222@gmail.com>
2022-05-13 18:39:33 -0400

    Initial Commit

---
+++ SuperMario64/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  SuperMario64
+//
+//  Created by Eric Tu on 5/13/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
